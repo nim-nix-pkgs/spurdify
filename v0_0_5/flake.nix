b10a97661286e88fa43f645110859629b156c9c8
hugosenari <hugosenari@gmail.com>
2022-03-22 00:37:59 -0300

    chore: re-index ../nimpkgs/s/spurdify

--- v0_0_5/flake.nix
+++ v0_0_5/flake.nix
@@ -9,7 +9,7 @@
   
   inputs.src-spurdify-v0_0_5.flake = false;
   inputs.src-spurdify-v0_0_5.owner = "paradox460";
-  inputs.src-spurdify-v0_0_5.ref   = "refs/tags/v0.0.5";
+  inputs.src-spurdify-v0_0_5.ref   = "v0_0_5";
   inputs.src-spurdify-v0_0_5.repo  = "spurdify";
   inputs.src-spurdify-v0_0_5.type  = "github";
   
